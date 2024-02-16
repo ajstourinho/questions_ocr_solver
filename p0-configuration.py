2eb21ae8bda592699804cb25d601098d55fbcd74
ajstourinho <ajstourinho@gmail.com>
2024-02-16 17:05:54 -0300

    Revert "Correct names for importing"
    This reverts commit 5b899f60db0876f79cf8f917b2e1453c177e6fd8.

---
+++ p0-configuration.py
@@ -0,0 +1,2 @@
+API_KEY = "<your_openai_api_key"
+MAX_TOKENS_PER_API_CALL = 1000
--- p2-main.py
+++ p2-main.py
@@ -14,6 +14,8 @@ def encode_image(image_path):
   with open(image_path, "rb") as image_file:
     return base64.b64encode(image_file.read()).decode('utf-8')
 
+
+
 def gpt_request(image_path):
     print(f"Making request for OpenAI API...")
 
